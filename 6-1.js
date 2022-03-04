aa3109166a58e659e9df9fcc1eea5114b3e313d4
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-03-03 18:13:27 -0600

    Class 6 Exercises

---
+++ 6-1.js
@@ -0,0 +1,14 @@
+function calculateTriangleArea(x, y) {
+    return x * y / 2
+  }
+
+console.log(calculateTriangleArea(5,5));
+
+
+// let baseValue = Number(prompt("Enter the base of the triangle: "));
+// let heightValue = Number(prompt("Enter the height of the triangle: "));
+//  const areaValue = (baseValue * heightValue) /2;
+
+
+
+// console.log("The area of the triangle is " + areaValue);
---
+++ 6-2.js
@@ -0,0 +1,2 @@
+console.log(rangeBetwee(4, 7));
+[4, 5, 6, 7]
